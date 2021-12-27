af3dee0cdbad62754a806fc302c8e7b3bf0fc02a
Anastasia <nastre70@gmail.com>
2021-12-27 20:24:04 +1000

    Task checkings

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,4 +1,4 @@
-import java.util.Arrays;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -15,6 +15,25 @@ public class Main {
         Arrays.fill(charArray, 'f'); // заполнение массива статическими элементами
         System.out.println(charArray);
 
+        Collection col = new List() { // вставьте нужный код
+            // другие переопределенные методы скрыты для краткости
+            @Override
+            public Object set(int index, Object element) {
+                return null;
+            }
+            @Override
+            public int indexOf(Object o) {
+                return 0;
+            }
+        }
+    }
 
+    private static void collectionWork(Queue col) { // вставьте нужный код
+        Object polledObject = col.poll();
+    }
+
+    private static void collectionWork(Collection collection) {
+        Map col = (Map) collection; // вставьте нужный код
+        col.put("Element 1", new Object());
     }
 }
