89f020330c74474d4cadd8e7b04e00e2f9f163eb
Anastasia <nastre70@gmail.com>
2022-01-12 20:57:57 +1000

    Added task8

---
+++ Main/src/Task8/Main.java
@@ -0,0 +1,21 @@
+package Task8;
+
+import java.util.Map;
+import java.util.TreeMap;
+public class Main {
+    public static void main(String[] args) {
+        Map<Integer, String> winners = new TreeMap<> (); // ваш код тут
+
+        winners.put(3, "Вася");
+        winners.put(1, "Федя");
+        winners.put(2, "Антон");
+
+        printWinners(winners);
+    }
+
+    private static void printWinners(Map<Integer, String> winners) {
+        for (Map.Entry<Integer, String> entry : winners.entrySet()) {
+            System.out.println(entry.getKey() + " место занял " + entry.getValue());
+        }
+    }
+}
---
+++ Main/src/Task8/Person.java
@@ -0,0 +1,17 @@
+package Task8;
+
+public class Person {
+
+    private int id;
+    private String name;
+
+    public Person(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return id + " " + name;
+    }
+}
